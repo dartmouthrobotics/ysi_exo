0f5d281a4ee58130946be6f03be71f045123c29b
Alberto Quattrini Li <alberto.quattrini.li@dartmouth.edu>
2020-03-10 22:47:52 -0400

    Added a wrapper for reading CSV files from the Kor EXO and outputting in the same format as if it was being read from the DCP.

---
+++ src/ysi_exo/exo_csv_reader.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""Reader of the CSV log from the EXO multiparameter sonde.
+
+Reading values from the CSV log file export from Kor EXO.
+An example file (with header) is in the test folder.
+
+More details at:
+.. EXO manual (page 62):
+   https://www.ysi.com/File%20Library/Documents/Manuals/EXO-User-Manual-Web.pdf
+
+"""
+
+import io
+import enum
+import time
+
+from converter import Converter
+from parameter_codes import *
+from default_values import *
+
+
+class ExoCsvReader(Converter):
+    """Interface to read the log file.
+
+    Attributes:
+        parameters (list): Order of how parameters are interpreted.
+        data (list): array with data.
+    """
+    FIRST_LINE_SEP_INDEX = -2 #"sep="
+    ENCODING = 'utf_16_le'
+    SKIP_NUM_LINES = 8
+    TIME_SEP = ':'
+    DATE_SEP = '/'
+    DATE_ORDER = {\
+            DATE_YY : [0, 1, 2],
+            DATE_MM : [2, 0, 1],
+            DATE_DD : [2, 1, 0]
+    }
+
+    def __init__(self, csv_file_path):
+        """Initialization of the CSV reader.
+
+        Implementation assumes header.
+        
+        """
+        self.csv_file = io.open(csv_file_path, "r", encoding=self.ENCODING)
+
+        self.sep = self.csv_file.readline()[self.FIRST_LINE_SEP_INDEX]
+        self._skip_lines(self.SKIP_NUM_LINES)
+        self.parameters_line = self.csv_file.readline().split(self.sep)
+
+        # Initialization of the relevant entries.
+        self._initialize_read()
+
+    def read_parameters(self):
+        """Read parameters that are in the log file."""
+
+        self.parameters = []
+        self.parameters_indices = []
+        for i, parameter_type in enumerate(self.parameters_line):
+            try:
+                self.parameters.append(PARAM_STRINGS[parameter_type])
+                self.parameters_indices.append(i)
+            except KeyError:
+                continue
+
+        self.data = [None] * len(self.parameters)
+
+    def read_data(self):
+        """Read data."""
+
+        if self.parameters:
+            raw_data_list = self.csv_file.readline().split(self.sep)
+            for i in xrange(len(self.parameters)):
+                raw_data = raw_data_list[self.parameters_indices[i]]
+                if self.parameters[i] == TIME:
+                    self.data[i] = float(''.join(raw_data.split(self.TIME_SEP)))
+                elif DATE_DD <= self.parameters[i] <= DATE_YY:
+                    date_list = raw_data.split(self.DATE_SEP)
+                    self.data[i] = self._date_to_float(date_list, self.parameters[i])
+                else:
+                    self.data[i] = float(raw_data)
+
+    def _skip_lines(self, num_lines):
+        """Skip lines."""
+
+        for i in xrange(num_lines):
+            self.csv_file.next()
+
+    def _date_to_float(self, date_list, date_format):
+        """Transform date from string to float."""
+        order = self.DATE_ORDER[date_format]
+        new_date_list = [None] * 3
+        new_date_list[order[0]] = date_list[order[0]][-2:] # Year.
+        new_date_list[order[1]] = date_list[order[1]].zfill(2) # Month.
+        new_date_list[order[2]] = date_list[order[2]].zfill(2) # Day.
+        
+
+        return float(''.join(new_date_list))
+
+    def _determine_encoding(self, first_line):
+        """Determine encoding based on the first line (in bytes)."""
+        import magic
+        m = magic.open(magic.MAGIC_MIME_ENCODING)
+        m.load()
+        encoding = m.buffer(first_line)
+        bom= codecs.BOM_UTF16_LE
+        first_line = first_line[len(bom):]
+
--- src/ysi_exo/parameter_codes.py
+++ src/ysi_exo/parameter_codes.py
@@ -16,15 +16,22 @@ TIME = 54
 ODO_PERCSAT = 211
 ODO_M = 212
 TEMP_C = 1
+COND_U = 5
 SPCOND_U = 7
 SAL = 12
 TURBIDITY_FNU = 223
 TSS_M = 190
+PH_M = 17
 PH = 18
 ORP = 19
+TDS_M = 110
 PRESS_PSIA = 20
 DEPTH_M = 22
 CHLOROPHYLL_U = 193
+CHLOROPHYLL_RFU = 194
+ODO_PERC_SAT = 211
+ODO_PERC_LOCAL = 214
+ODO_MG = 212
 BGAPE_U = 226
 FDOM_RFU = 227
 TEMP_F = 2
@@ -33,8 +40,38 @@ BATTERY = 28
 NH3 = 47
 NH4 = 48
 CL_MG = 112
-BGAPC = 216
+BGAPC_RFU = 216
 NLFCOND_U = 238
+CABLE = 230
 
 DATE_FORMAT = {DATE_DD: "%d%m%y", DATE_MM: "%m%d%y", DATE_YY: "%y%m%d"}
 TIME_FORMAT = "%H%M%S"
+
+PARAM_STRINGS = {\
+    u'Date (MM/DD/YYYY)' : DATE_MM,
+    u'Date (DD/MM/YYYY)' : DATE_DD,
+    u'Date (YYYY/MM/DD)' : DATE_YY,
+    u'Time (HH:mm:ss)' : TIME,
+    #u'Time (Fract. Sec)' : , 
+    #u'Site Name' : ,
+    u'Chlorophyll RFU' : CHLOROPHYLL_RFU, 
+    u'Cond \xb5S/cm' : COND_U,
+    u'Depth m' : DEPTH_M, 
+    u'nLF Cond \xb5S/cm' : NLFCOND_U, 
+    u'ODO % sat' : ODO_PERC_SAT, 
+    u'ODO % local' : ODO_PERC_LOCAL, 
+    u'ODO mg/L' : ODO_MG, 
+    u'Pressure psi a' : PRESS_PSIA,
+    u'Sal psu' : SAL, 
+    u'SpCond \xb5S/cm' : SPCOND_U, 
+    u'TAL PC RFU' : BGAPC_RFU,
+    u'TDS mg/L' : TDS_M, 
+    u'Turbidity FNU' : TURBIDITY_FNU, 
+    u'TSS mg/L' : TSS_M,
+    u'pH' : PH,
+    u'pH mV' : PH_M, 
+    u'Temp \xb0C' : TEMP_C, 
+    #u'Vertical Position m' : , 
+    u'Battery V' : BATTERY, 
+    u'Cable Pwr V' : CABLE
+}
