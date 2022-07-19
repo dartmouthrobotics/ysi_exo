8c041b77bfb09bbb6bb14c4d5301d91b1f4bf10e
jakebonney10 <43123441+jakebonney10@users.noreply.github.com>
2022-07-19 12:16:31 -0400

    Update utilities.py

--- src/ysi_exo/utilities.py
+++ src/ysi_exo/utilities.py
@@ -1,5 +1,5 @@
 import time
-from parameter_codes import *
+from ysi_exo.parameter_codes import *
 
 def convert_date_to_epoch(date_value, time_value, date_parameter_code=DATE_YY):
     """Convert date and time to epoch.
